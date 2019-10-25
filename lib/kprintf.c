fca2b4d1d6e65545d351f7d406cd47f85d17e2f3
cjyxxxc <cjyxxxc@gmail.com>
2019-10-25 11:03:57 +0200

    still editing

--- lib/kprintf.c
+++ lib/kprintf.c
@@ -40,81 +40,89 @@ void intToStr(char n){
 }
 
 
-int kprintf(char type, ...){
-    va_list v1;
-    va_start(v1,type);
+int kprintf(char * Str, ...){
     sendChar('\n');
-
-    char ch = (char)va_arg(v1,int);
-    char * str;
-    switch(type)
-    {
-        case 'c':
-            sendChar(ch);
-           str  = " is as a char: ";
-            while (str[0] != 0){
-                sendChar(str[0]);
-                str++;
+    int i=0, j=0;
+    va_list v1;
+    va_start(v1,str);
+    // char ch = (char)va_arg(v1,int);
+    char buff[100]={0},tmp[20];
+    while(str && str[i]){
+        if(str[i] == '%'){
+            i++;
+            switch(str)
+            {
+                case 'c':
+                    sendChar(str[i]);
+                    i++;
+                    break;
+                case 's':
+                    buff[j] = (unsigned char) var_arg(v1,int);
+                    // TODO : implement sprintf or itoa
+                    sprintf(va_arg(v1,int), tmp, 16);
+                    // TODO : implement strcpy
+                    strcpy(&buff[j], tmp);
+                    // TODO : implement strlen
+                    j+= strlen(tmp);
+                    while (buff[0] != 0){
+                        sendChar(buff[0]);
+                        buff++;
+                    }
+                    break;        
+                case 'x':
+                    sendChar(ch);
+                    char * str = " is in hexadecimal: 0x";
+                    while (str[0] != 0){
+                        sendChar(str[0]);
+                        str++;
+                    }
+                    decToHexStr(ch);
+                    break; 
+                case 'i':
+                    sendChar(ch);
+                    str = " is as integer: ";
+                    while (str[0] != 0){
+                        sendChar(str[0]);
+                        str++;
+                    }
+                    intToStr(ch);
+                    break;
+                case 'u':
+                    sendChar(ch);
+                    str = " is as unsigned integer: ";
+                    while (str[0] != 0){
+                        sendChar(str[0]);
+                        str++;
+                    }
+                    unsigned int u_ch = ch;
+                    intToStr(u_ch);
+                    break;
+                case 'p':
+                    sendChar(ch);
+                    str = " is at address : 0x";
+                    while (str[0] != 0){
+                        sendChar(str[0]);
+                        str++;
+                    }
+                    decToHexStr(ch);
+                    break;
+                case '%':
+                    sendChar('%');
+                    break;
+                default:
+                    str = "krpintf error : wrong option !! please specify one of the following character: c,s,x,i,u,p,%\n";
+                    while (str[0] != 0){
+                        sendChar(str[0]);
+                        str++;
+                        return -1;
+                    }
             }
-            sendChar(ch);
-            break;
-        case 's':
-            sendChar(ch);
-            str = " is as a string: ";
-            while (str[0] != 0){
-                sendChar(str[0]);
-                str++;
-            }
-            sendChar(ch);
-            break;        
-        case 'x':
-            sendChar(ch);
-            char * str = " is in hexadecimal: 0x";
-            while (str[0] != 0){
-                sendChar(str[0]);
-                str++;
-            }
-            decToHexStr(ch);
-            break; 
-        case 'i':
-            sendChar(ch);
-            str = " is as integer: ";
-            while (str[0] != 0){
-                sendChar(str[0]);
-                str++;
-            }
-            intToStr(ch);
-            break;
-        case 'u':
-            sendChar(ch);
-            str = " is as unsigned integer: ";
-            while (str[0] != 0){
-                sendChar(str[0]);
-                str++;
-            }
-            unsigned int u_ch = ch;
-            intToStr(u_ch);
-            break;
-        case 'p':
-            sendChar(ch);
-            str = " is at address : 0x";
-            while (str[0] != 0){
-                sendChar(str[0]);
-                str++;
-            }
-            decToHexStr(ch);
-            break;
-        case '%':
-            sendChar('%');
-            break;
-        default:
-            str = "krpintf error : wrong option !! please specify one of the following character: c,s,x,i,u,p,%\n";
-            while (str[0] != 0){
-                sendChar(str[0]);
-                str++;
-                return -1;
-            }
+            return 0;
+        }
+        else{
+            sendChar(str[i]);
+            i++;
+        }
     }
-    return 0;
     
 }
